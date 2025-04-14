0998e71095ded1fba7069241f09ecb4146f703f8
Yefri <yefrioscarlaura@gmail.com>
2025-04-14 13:03:59 -0500

    promotions is stable to use

--- server/api/root.ts
+++ server/api/root.ts
@@ -1,8 +1,10 @@
 import { router } from '../trpc'
 import { proformaRouter } from './routers/proforma'
+import { promotionsRouter } from './routers/promotions'
 
 export const appRouter = router({
   proforma: proformaRouter,
+  promotions: promotionsRouter,
 })
 
 export type AppRouter = typeof appRouter 
